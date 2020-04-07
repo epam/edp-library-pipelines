6a6125ce9e95b48190fdfaaf0e75bc20d527859d
Artyom Gatalsky <artyom_gatalsky@epam.com>
2020-04-07 10:59:29 +0300

    [EPMDEDP-4939] variable for additional maven args
    Change-Id: If09d67612c782489cde43ea8b992abaecccdca79

--- src/com/epam/edp/buildtool/Maven.groovy
+++ src/com/epam/edp/buildtool/Maven.groovy
@@ -38,11 +38,12 @@ class Maven implements BuildTool {
         this.releasesPath = job.getParameterValue("ARTIFACTS_RELEASES_PATH", "edp-maven-releases")
         this.hostedPath = job.getParameterValue("ARTIFACTS_HOSTED_PATH", "edp-maven")
         this.groupPath = job.getParameterValue("ARTIFACTS_PUBLIC_PATH", "edp-maven-group")
+        this.additionalArgs = job.getParameterValue("ADDITIONAL_BUILDTOOL_ARGS", "")
         this.settings = writeSettingsFile(this.script.libraryResource("maven/settings.xml"))
         this.groupRepository = "${nexus.repositoriesUrl}/${this.groupPath}"
         this.hostedRepository = "${nexus.repositoriesUrl}/${this.hostedPath}"
         this.command = "mvn --settings ${this.settings}"
-        this.properties = "-Dartifactory.baseUrl=${nexus.baseUrl} -Dartifactory.releasePath=${this.releasesPath} -Dartifactory.snapshotsPath=${this.snapshotsPath} -Dartifactory.groupPath=${this.groupPath}"
+        this.properties = "-Dartifactory.baseUrl=${nexus.baseUrl} -Dartifactory.releasePath=${this.releasesPath} -Dartifactory.snapshotsPath=${this.snapshotsPath} -Dartifactory.groupPath=${this.groupPath} ${this.additionalArgs}"
     }
 
     private writeSettingsFile(fileContent) {
