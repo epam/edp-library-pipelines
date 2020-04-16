944b7bb9d22b2893a8a4d408b9b674d8e4ce654e
Viktor_Voronin <viktor_voronin@epam.com>
2020-04-16 17:35:48 +0300

    [EPMDEDP-TECH] Fix SetDescription
    Change-Id: I8665bb8e954f22b3ba5b34cbb60f12f64a31045d

--- src/com/epam/edp/Job.groovy
+++ src/com/epam/edp/Job.groovy
@@ -311,7 +311,7 @@ class Job {
     }
 
     def setDescription(description, addDescription = false) {
-        if (addDescription && script.currentBuild.description.length() > 0)
+        if (addDescription && script.currentBuild.description?.trim())
             script.currentBuild.description = "${script.currentBuild.description}\r\n${description}"
         else
             script.currentBuild.description = description
