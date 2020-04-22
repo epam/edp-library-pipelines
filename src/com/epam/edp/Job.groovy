10b525dc87ac572c0567ce6a3c96ee6ad99b7f28
Viktor Semenov <Viktor_Semenov2@epam.com>
2020-04-22 10:44:59 +0300

    [EPMDEDP-5020] Add timeout for manual approve stage
    Change-Id: Iafa1939da2efda1afc9a2450b17b6bf149046e63
    (cherry picked from commit 7b6364733f2dd6a4972bf36959cd84eeb2a03198)

--- src/com/epam/edp/Job.groovy
+++ src/com/epam/edp/Job.groovy
@@ -60,6 +60,7 @@ class Job {
     def crApiVersion = "v2"
     def crApiGroup
     def dnsWildcard
+    def manualApproveStageTimeout
 
     Job(type, platform, script) {
         this.type = type
@@ -121,6 +122,7 @@ class Job {
         this.deployProject = "${this.edpName}-${this.pipelineName}-${stageName}"
         this.ciProject = getParameterValue("CI_NAMESPACE")
         this.deployTimeout = getParameterValue("DEPLOY_TIMEOUT", "300s")
+        this.manualApproveStageTimeout = getParameterValue("MANUAL_APPROVE_TIMEOUT", "10")
         stageContent.applications.each() { item ->
             stageCodebasesList.add(item.name)
             codebaseBranchList["${item.name}"] = ["branch"  : item.branchName,
