3124ca80c6d169909ca8092c0e2a3b2f8ffe1dad
Alexander Morozov <alexander_morozov@epam.com>
2020-04-30 14:36:20 +0300

    [EPMDEDP-5042] Add publishing status of the build to GitLab
    Change-Id: Icb2cde9494ba8627a919696d3c10cf2ece8463b1

--- vars/CodeReview.groovy
+++ vars/CodeReview.groovy
@@ -27,6 +27,7 @@ import org.apache.commons.lang.RandomStringUtils
 def call() {
     def context = [:]
     node("master") {
+        updateGitlabCommitStatus name: 'Jenkins', state: 'running'
         stage("Init") {
             context.platform = new PlatformFactory().getPlatformImpl(this)
 
@@ -81,4 +82,5 @@ def call() {
             }
         }
     }
+    updateGitlabCommitStatus name: 'Jenkins', state: "success"
 }
